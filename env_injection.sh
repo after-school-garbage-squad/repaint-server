c8dbf73f0a6903f73560e353dba58819e8a1879f
shiron <shiron4710.dev@gmail.com>
2023-08-07 21:25:32 +0900

    feat: Integrate
    Integrate environment variable injection into docker compose

--- env_injection.sh
+++ env_injection.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 tmpfile="/tmp/tempfile_$RANDOM"
 
 cat $1 > $tmpfile
 
-if [ -f .env ]; then
-  while read -r line || [[ -n "$line" ]]; do
-    key="${line%%=*}"
-    value="${line#*=}"
+for env_var in $(printenv); do
+  key=$(echo "$env_var" | cut -d '=' -f1)
+  value=$(echo "$env_var" | cut -d '=' -f2-)
 
-    value="${value#"${value%%[![:space:]]*}"}"
-    value="${value%"${value##*[![:space:]]}"}"
-    
-    sed -i -e s/{{$key}}/$value/g $tmpfile
-  done < .env
+  value="${value#"${value%%[![:space:]]*}"}"
+  value="${value%"${value##*[![:space:]]}"}"
+  value=$(echo "$value" | sed 's/\//\\\//g')
+
+  sed -i -e s/{{$key}}/$value/g $tmpfile
+done
+
+if [ $# -ge 2 ]; then
+  mv "$tmpfile" "$2"
 else
-  echo "エラー: .envファイルが見つかりません"
-  exit 1
+  cat "$tmpfile"
+  rm "$tmpfile"
 fi
-
-cat "$tmpfile"
-
-rm "$tmpfile"
