da6641661db4ecda64a16541ea68154ba5a90042
Rintaro Itokawa <me@re-taro.dev>
2023-08-30 18:41:27 +0900

    test: add visitor repository test

--- core/src/impls/visitor.rs
+++ core/src/impls/visitor.rs
@@ -72,3 +72,79 @@ impl VisitorRepository for SeaOrm {
             .collect()
     }
 }
+
+#[cfg(test)]
+pub(crate) mod test {
+    use pretty_assertions::*;
+
+    use crate::TestingSeaOrm;
+
+    use super::*;
+
+    impl TestingSeaOrm {
+        pub(crate) async fn make_test_visitor(&self, event_id: i32) -> Visitor {
+            let visitor = visitors::ActiveModel {
+                event_id: Set(event_id),
+                visitor_id: Set(Id::new().dty()),
+                registration_id: Set("testtesttest".into()),
+                ..Default::default()
+            }
+            .insert(self.orm().con())
+            .await
+            .unwrap();
+
+            to_model(visitor).unwrap()
+        }
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn test_get() {
+        let orm = TestingSeaOrm::new().await;
+        let event = orm.make_test_event().await;
+        let visitor = orm.make_test_visitor(event.id).await;
+
+        let res = VisitorRepository::get(orm.orm(), event.id, visitor.visitor_id)
+            .await
+            .unwrap();
+
+        self::assert_eq!(res, Some(visitor));
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn test_list() {
+        async fn test(q: u8) {
+            let orm = TestingSeaOrm::new().await;
+            let event = orm.make_test_event().await;
+            let mut visitors = Vec::<Visitor>::new();
+            for _ in 0..q {
+                visitors.push(orm.make_test_visitor(event.id).await);
+            }
+
+            let res = VisitorRepository::list(orm.orm(), event.id).await.unwrap();
+
+            self::assert_eq!(res, visitors);
+        }
+
+        test(1).await;
+        test(2).await;
+        test(3).await;
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn test_delete() {
+        let orm = TestingSeaOrm::new().await;
+        let event = orm.make_test_event().await;
+        let mut visitors = Vec::<Visitor>::new();
+        for _ in 0..3 {
+            visitors.push(orm.make_test_visitor(event.id).await);
+        }
+        let _ = VisitorRepository::delete(orm.orm(), visitors[1].id)
+            .await
+            .unwrap();
+        visitors.remove(1);
+
+        let res = VisitorRepository::list(orm.orm(), event.id).await.unwrap();
+
+        self::assert_eq!(res, visitors);
+    }
+}
