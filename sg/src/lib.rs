a6fbb4069a939e1161df7dea2711398d9b7a64ba
Rintaro Itokawa <me@re-taro.dev>
2023-09-09 17:56:58 +0900

    feat: implement send-grid

--- sg/src/lib.rs
+++ sg/src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![warn(missing_debug_implementations)]
+#![warn(unreachable_pub)]
+
+use async_trait::async_trait;
+use email_address::EmailAddress;
+use repaint_server_usecase::infra::email::EmailSender;
+use sendgrid::error::SendgridError;
+use sendgrid::v3::{Content, Email, Message, Personalization, Sender};
+use teloc::dev::DependencyClone;
+
+#[derive(Debug, Clone)]
+pub struct SendGrid {
+    sender: Sender,
+    send_from: EmailAddress,
+    url: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl SendGrid {
+    pub fn new(api_key: &str, send_from: EmailAddress, url: &str) -> Self {
+        let sender = Sender::new(api_key.to_string());
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Self {
+            sender,
+            send_from,
+            url: url.to_string(),
+        }
+    }
+}
+
+impl DependencyClone for SendGrid {}
+
+#[async_trait]
+impl EmailSender for SendGrid {
+    type Error = SendgridError;
+
+    async fn send(&self, to: EmailAddress, token: String) -> Result<(), Self::Error> {
+        let personalization = Personalization::new(Email::new(to.to_string()));
+        let message = Message::new(Email::new(self.send_from.to_string()))
+            .set_subject("【Re:paint】管理者への追加のお知らせ")
+            .add_content(
+                Content::new()
+                    .set_content_type("text/plain")
+                    .set_value(format!(
+                        include_str!("./message.tmp.txt"),
+                        URL = self.url,
+                        TOKEN = token
+                    )),
+            )
+            .add_personalization(personalization);
+        self.sender.send(&message).await?;
+
+        Ok(())
     }
 }
