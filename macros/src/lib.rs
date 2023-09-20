531a550d880e60fca126c707da02ed40adb25c67
Rintaro Itokawa <me@re-taro.dev>
2023-09-20 14:06:48 +0900

    feat: implement warp_path macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![warn(missing_debug_implementations)]
+#![warn(unreachable_pub)]
+#![recursion_limit = "1000"]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use proc_macro::TokenStream;
+use proc_macro_error::proc_macro_error;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+mod warp_path;
+
+#[proc_macro_error]
+#[proc_macro]
+pub fn wpath(input: TokenStream) -> TokenStream {
+    warp_path::warp_path(input)
 }
