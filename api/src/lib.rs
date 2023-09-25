5e415fe385c9b9ca754e623f8a493ac963ac838f
Rintaro Itokawa <me@re-taro.dev>
2023-09-25 19:27:56 +0900

    refactor: move utils to utils.rs

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,23 +1,21 @@
 use std::net::{SocketAddr, SocketAddrV4};
-use std::str::FromStr;
 use std::time::Duration;
 
-use axum::extract::FromRef;
 use repaint_server_core::SeaOrm;
 use teloc::ServiceProvider;
 use tokio::signal;
 use tokio::sync::oneshot;
 
+use crate::utils::{envvar, envvar_str};
+
 mod middleware;
 mod routes;
+mod utils;
 
 #[cfg(debug_assertions)]
 const LOG_LEVEL: &str = "debug,sqlx=warn";
 #[cfg(not(debug_assertions))]
 const LOG_LEVEL: &str = "info,sqlx=warn";
-
-#[derive(Debug, Clone, FromRef)]
-struct AppState {}
 
 pub async fn run() {
     let addr: SocketAddr = SocketAddr::V4(SocketAddrV4::new(
@@ -25,7 +23,6 @@ pub async fn run() {
         envvar("PORT", 4000),
     ));
     let db_url = envvar_str("DATABASE_URL", None);
-    let base_url = envvar_str("AUTHORITY", None);
     let db = SeaOrm::new(db_url).await.expect("couldn't connect to db");
 
     let container = ServiceProvider::new()
@@ -75,49 +72,4 @@ async fn wait_for_shutdown_signal(force_shutdown_tx: oneshot::Sender<()>) {
         tokio::time::sleep(Duration::from_secs(3)).await;
         force_shutdown_tx.send(()).unwrap();
     });
-}
-
-fn envvar<T, E>(var_name: &str, default: impl Into<Option<T>>) -> T
-where
-    T: std::fmt::Debug + FromStr<Err = E>,
-    E: std::fmt::Debug,
-{
-    if let Ok(v) = std::env::var(var_name) {
-        if v.is_empty() {
-            panic!("{var_name} is set, but it's empty");
-        }
-
-        return v
-            .parse()
-            .unwrap_or_else(|e| panic!("environment variable {var_name} is not valid: {e:?}"));
-    }
-
-    if !cfg!(debug_assertions) {
-        panic!("{var_name} is not set; Default environment variables are not allowed in the release build");
-    }
-    let Some(d) = default.into() else {
-        panic!("{var_name} is not set");
-    };
-
-    tracing::warn!("environment variable {var_name} is not set. defaulting to {d:?}");
-    d
-}
-
-fn envvar_str<'a>(var_name: &str, default: impl Into<Option<&'a str>>) -> String {
-    if let Ok(v) = std::env::var(var_name) {
-        if v.is_empty() {
-            panic!("{var_name} is set, but it's empty");
-        }
-
-        return v;
-    }
-    if !cfg!(debug_assertions) {
-        panic!("{var_name} is not set; Default environment variables are not allowed in the release build");
-    }
-    let Some(d) = default.into() else {
-        panic!("{var_name} is not set");
-    };
-    tracing::warn!("environment variable {var_name} is not set. defaulting to {d:?}");
-
-    d.to_owned()
 }
