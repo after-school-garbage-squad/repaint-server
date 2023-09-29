d9f6c35f1419bf163a581f4bfe230d616e2b9629
Rintaro Itokawa <me@re-taro.dev>
2023-09-29 17:28:01 +0900

    feat: impl gmail crate

--- gmail/src/lib.rs
+++ gmail/src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![warn(unreachable_pub)]
+
+use std::str::FromStr;
+
+use async_trait::async_trait;
+use email_address::EmailAddress;
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::transport::smtp::Error;
+use lettre::{Message, SmtpTransport, Transport};
+use repaint_server_usecase::infra::email::EmailSender;
+use teloc::dev::DependencyClone;
+
+#[derive(Clone)]
+pub struct Gmail {
+    client: SmtpTransport,
+    send_from: EmailAddress,
+    url: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Gmail {
+    pub fn new(send_from: String, username: String, password: String, url: String) -> Self {
+        let send_from = EmailAddress::from_str(send_from.as_str()).expect("invalid email address");
+        let cred = Credentials::new(username, password);
+        let client = SmtpTransport::relay("smtp.gmail.com")
+            .expect("failed to relay smtp.gmail.com")
+            .credentials(cred)
+            .build();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Self {
+            client,
+            send_from,
+            url,
+        }
+    }
+}
+
+impl DependencyClone for Gmail {}
+
+#[async_trait]
+impl EmailSender for Gmail {
+    type Error = Error;
+
+    async fn send(&self, to: EmailAddress, token: String) -> Result<(), Self::Error> {
+        let mail = Message::builder()
+            .from(self.send_from.to_string().parse().unwrap())
+            .to(to.to_string().parse().unwrap())
+            .subject("【Re:paint】管理者への追加のお知らせ")
+            .header(ContentType::TEXT_PLAIN)
+            .body(format!(
+                include_str!("./message.tmp.txt"),
+                URL = self.url,
+                TOKEN = token
+            ))
+            .expect("failed to build message");
+        self.client.send(&mail)?;
+
+        Ok(())
     }
 }
