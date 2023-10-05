47555b453e80b10d78ff04e110b8ebd290b7781e
Rintaro Itokawa <me@re-taro.dev>
2023-10-05 11:27:24 +0900

    feat: change id field type to Option<T>

--- usecase/src/model/otp.rs
+++ usecase/src/model/otp.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Token {
     pub full: String,
-    pub id: i32,
+    pub id: Option<i32>,
     pub token: String,
     pub url: String,
     pub limit_time: i32,
