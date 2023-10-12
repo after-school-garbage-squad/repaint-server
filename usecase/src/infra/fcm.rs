bb4963779fdf313b0e529defa06ad1ce9b16761e
Rintaro Itokawa <me@re-taro.dev>
2023-10-13 07:59:51 +0900

    feat(refactor): remove fcm wrapper and use pubsub for notification (#364)
    * chore: format code
    
    * chore: ***printf debug***
    
    * chore: fic debug
    
    * feat(refactor): remove fcm wrapper and use pubsub for notification
    
    * chore: update licenses

--- usecase/src/infra/fcm.rs
+++
@@ -1,9 +0,0 @@
-use async_trait::async_trait;
-use repaint_server_model::{AsyncSafe, StaticError};
-
-#[async_trait]
-pub trait FirebaseCloudMessaging: AsyncSafe {
-    type Error: StaticError;
-
-    async fn send(&self, registration_id: String, spot_name: String) -> Result<(), Self::Error>;
-}
